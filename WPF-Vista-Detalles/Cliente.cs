a6da279124c7cbc1cb44f569c8096d15dcbd685f
a15carlosmpf <miguel@ole.com>
2017-03-02 10:09:36 +0100

    Fin de la app!

---
+++ WPF-Vista-Detalles/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Vista_Detalles
+{
+    public class Cliente
+    {
+
+
+        public String dni { get; set; }
+        public String nombre { get; set; }
+        public String telefono { get; set; }
+        public String contacto { get; set; }
+        public String mail { get; set; }
+
+        public Cliente() { }
+
+        public Cliente(String dni, String nombre, String telefono, String contacto, String mail)
+        {
+            this.dni = dni;
+            this.nombre = nombre;
+            this.telefono = telefono;
+            this.contacto = contacto;
+            this.mail = mail;
+        }
+    }
+}
--- WPF-Vista-Detalles/MainWindow.xaml.cs
+++ WPF-Vista-Detalles/MainWindow.xaml.cs
@@ -23,6 +23,41 @@ namespace WPF_Vista_Detalles
         public MainWindow()
         {
             InitializeComponent();
+            leerDatos();
+        }
+
+        public void leerDatos()
+        {
+            System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(List<Cliente>));
+            System.IO.FileStream stream = System.IO.File.OpenRead("clientes.xml");
+            List<Cliente> lista = (List<Cliente>)serializer.Deserialize(stream);
+
+            for(int i = 0; i < lista.Count; i++)
+            {
+                listaClientes.Items.Add(lista[i]);
+            }
+            listaClientes.DisplayMemberPath = "nombre";
+            
+        }
+
+        private void listaClientes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count > 0)
+            {
+                Cliente cliente = (Cliente)e.AddedItems[0];
+                txtDni.Content = cliente.dni;
+                txtNombre.Content = cliente.nombre;
+                txtTelefono.Content = cliente.telefono;
+                txtContacto.Content = cliente.contacto;
+                txtMail.Content = cliente.mail;
+            }
+        }
+
+        private void btnBorrar_Click(object sender, RoutedEventArgs e)
+        {
+            
+                listaClientes.Items.Remove(listaClientes.SelectedItem);
+            
         }
     }
 }
